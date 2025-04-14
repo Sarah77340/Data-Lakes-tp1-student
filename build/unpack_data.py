55e40524c06bea853c538cbaaf7b4269633733d8
Sarah Nguyen <101677436+Sarah77340@users.noreply.github.com>
2025-04-14 10:29:09 +0000

    Exo2

--- build/unpack_data.py
+++ build/unpack_data.py
@@ -11,28 +11,42 @@ def unpack_data(input_dir, output_file):
     """
 
     # Step 1: Initialize an empty list to store DataFrames
+    dataframes = list()
 
     # Step 2: Loop over files in the input directory
+    #directory = os.path.join("c:\\","path")
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            # Step 3: Check if the file is a CSV or matches a naming pattern
+            if file.endswith(".csv"):
+                # Step 4: Read the CSV file using pandas
+                df = pd.read_csv(file)
 
-        # Step 3: Check if the file is a CSV or matches a naming pattern
-
-        # Step 4: Read the CSV file using pandas
-
-        # Step 5: Append the DataFrame to the list
-
+                # Step 5: Append the DataFrame to the list
+                dataframes.append(df)
+            
     # Step 6: Concatenate all DataFrames
-
     # Step 7: Save the combined DataFrame to output_file
-
-    pass
-
+    dfs = pd.concat(dataframes)
+    dfs.to_csv(output_file, index=False)
+    
 
 if __name__ == "__main__":
+    # python build\unpack_data.py --input_dir ".\workspaces\Data-Lakes-tp1-student\data\bronze" --output_file ".\workspaces\Data-Lakes-tp1-student\data\bronze\out.csv"
+
     import argparse
+
+    ###
+    import os
+    current_directory = os.getcwd()
+    print("Current Working Directory:", current_directory)
+    ###
 
     parser = argparse.ArgumentParser(description="Unpack and combine protein data")
     parser.add_argument("--input_dir", type=str, required=True, help="Path to input directory")
     parser.add_argument("--output_file", type=str, required=True, help="Path to output combined CSV file")
+    
     args = parser.parse_args()
+    print(args.input_dir)
 
     unpack_data(args.input_dir, args.output_file)
